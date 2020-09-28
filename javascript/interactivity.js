fbb7adc72a2739b6d58985d2916967f8c87eb39c
User Name <alaarabie1172000@gmail.com>
2020-09-28 20:05:39 +0200

    Web

---
+++ javascript/interactivity.js
@@ -0,0 +1,15 @@
+ /*function ExampleJS(){
+        var jFirst = document.getElementById("name").value;
+        alert("Welcome " + jFirst );
+     }
+     window.onbeforeunload = confirmExit;
+  function confirmExit()
+  {
+    return "Your Message Goes Here";
+  }
+        function ExampleJS(){
+        var jFirst = document.getElementById("name").value;
+        alert("Welcome " + jFirst );
+     }
+     onclick="Myfunction()"
+     */
